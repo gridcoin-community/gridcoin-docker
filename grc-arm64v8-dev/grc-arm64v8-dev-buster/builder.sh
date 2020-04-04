73e4491210988ce5a5aac1863c9d7457b129b498
Richard Leckinger <richard.leckinger@riseup.net>
2020-04-04 16:48:05 +1300

    add grc-arm64v8-buster

---
+++ grc-arm64v8-dev/grc-arm64v8-dev-buster/builder.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker login
+
+# build the build-deps stage:
+docker build --rm \
+    --target grc-arm64v8-build-deps-buster \
+    --cache-from=grc-arm64v8-build-deps-buster \
+    --tag grc-arm64v8-build-deps-buster .
+
+# build the devtime stage, using cached build-deps stage:
+docker build --rm \
+    --target grcarm64v8devbuster \
+    --cache-from=grc-arm64v8-build-deps-buster \
+    --tag gridcoincommunity/grc-arm64v8-dev:buster .
+
+# push the new version:
+docker push gridcoincommunity/grc-arm64v8-dev:buster
